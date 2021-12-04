f522960494e5d594c2d308e129c7faab7347e1b6
zchen665 <azchen665@gmail.com>
2021-12-04 15:51:17 -0600

    init classes

---
+++ sw/simulator.py
@@ -0,0 +1,15 @@
+class Simulator:
+    def __init__():
+      print("simulator starts")
+
+
+class Thread:
+    def __init__():
+      print("gg")
+  
+
+if __name__ == '__main__':
+  print('Welcome to ThreadKraken Simulator')
+  
+
+
