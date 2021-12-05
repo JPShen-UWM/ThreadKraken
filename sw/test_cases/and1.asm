3d7bc4c11a946efbbf1a0a650f184d7f08298db8
zchen665 <azchen665@gmail.com>
2021-12-05 14:46:21 -0600

    add bash script

--- sw/test_cases/and1.asm
+++ sw/test_cases/and1.asm
@@ -3,7 +3,4 @@ addi r22 r0 0xF0
 and r23 r21 r22
 
 #halt
-add r0 r0 r0
-add r0 r0 r0
-add r0 r0 r0
-add r0 r0 r0
+kill r0
--- sw/test_cases/not1.asm
+++ sw/test_cases/not1.asm
@@ -1,7 +1,4 @@
 add r21 r0 r0
 not r21 r21
 #halt
-add r0 r0 r0
-add r0 r0 r0
-add r0 r0 r0
-add r0 r0 r0
+kill r0
