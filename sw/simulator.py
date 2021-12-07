c1cc44b7675340b05f57edd87e822d8ad104732e
zchen665 <azchen665@gmail.com>
2021-12-07 03:32:11 -0600

    update simulator

--- sw/simulator.py
+++ sw/simulator.py
@@ -1,5 +1,5 @@
 import copy
-
+import re
 
 IDLE = 0
 RUNNABLE = 1
@@ -20,12 +20,128 @@ def find_next_thrd(last, pool):
       return (pool[i],i)
   return None
 
+def str_to_int(str):
+  if '0x' in str:
+    return int(str,16)
+  return int(str)
+
+def twos_comp(val, bits):
+    """compute the 2's complement of int value val"""
+    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+        val = val - (1 << bits)        # compute negative value
+    return val                         # return positive value as is
+
 
 class Simulator:
-    instr_map = {}
+    def _add(thrd, cmd):
+      args = re.split(',| ', cmd)
+      [_, rd, ra, rb] = [_ for _ in args if len(_) > 0]
+      
+      rd,ra,rb = int(rd[1:]),int(ra[1:]),int(rb[1:])
+
+      thrd.regs[rd] = thrd.regs[ra] + thrd.regs[rb]
+      return
+
+    def _not(thrd, cmd):
+      args = re.split(',| ', cmd)
+      [_, rd, ra] = [_ for _ in args if len(_) > 0]
+      
+      rd,ra = int(rd[1:]),int(ra[1:])
+
+      thrd.regs[rd] = ~thrd.regs[ra]
+      return
+
+    def _and(thrd, cmd):
+      args = re.split(',| ', cmd)
+      [_, rd, ra, rb] = [_ for _ in args if len(_) > 0]
+      
+      rd,ra,rb = int(rd[1:]),int(ra[1:]),int(rb[1:])
+
+      thrd.regs[rd] = thrd.regs[ra] & thrd.regs[rb]
+      return
+
+    def _or(thrd, cmd):
+      args = re.split(',| ', cmd)
+      [_, rd, ra, rb] = [_ for _ in args if len(_) > 0]
+      
+      rd,ra,rb = int(rd[1:]),int(ra[1:]),int(rb[1:])
+
+      thrd.regs[rd] = thrd.regs[ra] | thrd.regs[rb]
+      return
+
+    def _xor(thrd, cmd):
+      args = re.split(',| ', cmd)
+      [_, rd, ra, rb] = [_ for _ in args if len(_) > 0]
+      
+      rd,ra,rb = int(rd[1:]),int(ra[1:]),int(rb[1:])
+
+      thrd.regs[rd] = thrd.regs[ra] ^ thrd.regs[rb]
+      return
+
+    def _addi(thrd, cmd):
+      IMM_LEN = 12
+
+      args = re.split(',| ', cmd)
+      [_, rd, ra, imm] = [_ for _ in args if len(_) > 0]
+      
+      rd,ra= int(rd[1:]),int(ra[1:])
+      imm = twos_comp(str_to_int(imm), IMM_LEN)
+
+      thrd.regs[rd] = thrd.regs[ra] + imm
+      return
+
+    def _lbi(thrd, cmd):
+      args = re.split(',| ', cmd)
+      [_, rd, imm] = [_ for _ in args if len(_) > 0]
+      
+      rd,imm = int(rd[1:]), str_to_int(imm)
+
+      thrd.regs[rd] = (thrd.regs[rd]&~0xFFFF) + imm
+      return
+
+    def _slbi(thrd, cmd):
+      args = re.split(',| ', cmd)
+      [_, rd, imm] = [_ for _ in args if len(_) > 0]
+      
+      rd,imm = int(rd[1:]), str_to_int(imm)
+
+      thrd.regs[rd] = (thrd.regs[rd] << 16) + imm
+      return
+
+    cmd_table = ['add', 'not', 'and', 'or', 'xor', 'addi', 'andi', 'ori', 'xori',
+    'shlt', 'shrt', 'lbi', 'slbi','st', 'ld', 'jal', 'jalr', 'beq', 'bneq', 'blt', 'slp', 'wk', 'kill','nt']
+
+    func_map = {
+      'add': _add, 
+      'not': _not,
+      'and': _and, 
+      'or': _or,
+      'xor': _xor, 
+      'addi': _addi, 
+      # 'andi': _andi, 
+      # 'ori': _ori, 
+      # 'xori': _xori,
+      # 'shlt': _shlt, 
+      # 'shrt': _shrt, 
+      # 'shra': _shra,
+      'lbi': _lbi, 
+      'slbi': _slbi,
+      # 'st': _st, 
+      # 'ld': _ld, 
+      # 'jal': _jal, 
+      # 'jalr': _jalr, 
+      # 'beq': _beq, 
+      # 'bneq': _bneq, 
+      # 'blt': _blt,
+      # 'slp': _slp,
+      # 'wk': _wk,
+      # 'kill': _kill,
+      # 'nt': _nt
+    }
 
     def __init__(self):
       print("simulator starts")
+      self.counter = 0
       self.mem = {}
       self.threads = [0]*8
       self.instr = []
@@ -35,8 +151,6 @@ class Simulator:
     def run(self, file_name):
       self.load_instr(file_name)
       self.threads[0] = Thread() #root thread
-      self.threads[1] = Thread(1) #root thread
-      self.threads[2] = Thread(2) #root thread
       self.execute_program()
       return 
 
@@ -50,14 +164,16 @@ class Simulator:
     
     def processLabels(self, cmd):
       if not cmd or cmd[0] == '#' or cmd[0] == '/' or cmd[0] == '/': return
+
       if cmd[0] == ".":
-          self.labels[cmd] = self.pc
+          self.labels[cmd] = self.counter
 
       else:
           if cmd.find('/') != -1:
             self.instr.append(cmd[:cmd.find('/')].strip())
           else:
             self.instr.append(cmd)
+            self.counter += 1
       return
 
     def execute_program(self):
@@ -74,17 +190,38 @@ class Simulator:
           self.last_exe_thrd_idx = indx
         
         
-        print(self.last_exe_thrd_idx)
+        print(cur_thrd)
+        print(cur_thrd.regs)
       return
 
     def execute_on_thread(self, thrd):
       cmd = self.instr[thrd.pc]
 
+      args = re.split(',| ', cmd)
+      opcode= args[0].lower()
+      atomic = False
+      
+      
+      # print('pc %d -> cmd %s' %(cmd[0], opcode))
+      if opcode[-1] == 'a' and opcode[:-1] in Simulator.cmd_table:
+        if opcode[:-1] in ['jal', 'jalr', 'beq', 'bneq', 'blt']: # check if labels are used
+          retStr = Simulator.func_map[opcode[:-1]](thrd, cmd.lower(), self.labels)
+        else: 
+          retStr = Simulator.func_map[opcode[:-1]](thrd, cmd.lower())
+        atomic = True
+      else:
+        if opcode in ['jal', 'jalr', 'beq', 'bneq', 'blt']: # check if labels are used
+          retStr = Simulator.func_map[opcode](thrd, cmd.lower(), self.labels)
+        else:
+          retStr = Simulator.func_map[opcode](thrd, cmd.lower())
+
 
       thrd.pc += 1
       if thrd.pc >= len(self.instr):
         thrd.die()
-      return
+        return
+      if atomic: self.execute_on_thread(thrd)
+      return 
       
 
 
@@ -131,7 +268,11 @@ class Thread:
 if __name__ == '__main__':
   print('Welcome to ThreadKraken Simulator')
   s = Simulator()
-  s.run('./test_cases/add1.asm')
+  s.run('./test_cases/test_input.asm')
+
+  x = '0xF0'
+  x = twos_comp(str_to_int(x), 12)
+  print(x)
   
 
 
