1d7e0727e5aa7f779ce30ffa87b3900b5ade4358
tyee2 <tyee2@wisc.edu>
2021-12-12 17:49:02 -0600

    Update i_cache.sv

--- rtl/main/i_cache.sv
+++ rtl/main/i_cache.sv
@@ -43,37 +43,6 @@ module i_cache(
                 mem[index+i] <= {1'b1,wr_ins[i]};
         end
     
-/*     // read/write must be in range of valid addresses
-    always_comb begin
-        // default outputs
-        seg_f_en = 0;
-        
-        unique case(cur_pc) inside
-            // instr memory in range x0001_0000 to x0001_01FF
-            [32'h10000:32'h101FF]: begin
-                seg_f_en = 0;
-            end
-            // else outside accessible range: seg fault
-            default: begin
-                seg_f_en = 1;
-            end
-        endcase
-    end
-    
-    // seg. fault is unrecoverable
-    always_ff @(posedge clk, negedge rst_n)
-        if(!rst_n)
-            i_cache_seg_fault <= 0;
-        else if((rd_en|wr_en) && seg_f_en)
-            i_cache_seg_fault <= 1; 
-
-    // cache miss
-    // assign i_miss = (rd_en && ~mem[index][32]);
-*/
-
-    // atomic ins.
-    assign atomic = ins[0];
-    
     // return line of data
     assign line = mem[index];
     assign vld = line[32];
